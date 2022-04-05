8bfe2cf1407962a2a0ec4dd52b08e1b24d1b6f41
Shubham0997 <Shubham49412@gmail.com>
2022-04-06 00:10:01 +0530

    Final Commit

--- js/adminOnUserFunctions.js
+++ js/adminOnUserFunctions.js
@@ -122,3 +122,26 @@ function closeForm() { //function to close the popup form to update user values
   var blur2 =document.getElementById('searchUser');//unblur the background
   blur2.classList.toggle('activeForm')
 }
+
+//search functionality over users table
+function searchUser() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
